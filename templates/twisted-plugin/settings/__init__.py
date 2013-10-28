c626de3dcea5d021b74589dc0134dce69f2f5aa6
huyx <ycyuxin@gmail.com>
2013-10-28 09:12:14 +0800

    缺省不生成项目自定义配置文件，而是提示用户可以自行创建

--- templates/twisted-plugin/settings/__init__.py
+++ templates/twisted-plugin/settings/__init__.py
@@ -1,3 +1,9 @@
 # -*- coding: utf-8 -*-
-from default import *
-from {{project_name}} import *
+from .default import *
+
+try:
+    from .{{project_name}} import *
+except ImportError:
+    import os
+    filename = os.path.normpath('settings/{{project_name}}.py')
+    print u'提示: 可以通过修改 %s 进行配置' % filename
